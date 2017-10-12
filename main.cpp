9ea3b154ecb5c3f93af82ae50d149f02fc9603c9
fjavierponce <fco.poncecruz@gmail.com>
2017-10-12 11:50:39 -0500

    Initializing workspace for algorithm study examples.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm-study
+//
+//  Created by Francisco Ponce on 10/12/17.
+//  Copyright © 2017 Francisco Ponce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
