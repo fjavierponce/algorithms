3b7709832cb0a81f6d74c12596214b82f7540e31
fjavierponce <fco.poncecruz@gmail.com>
2017-11-06 22:49:00 -0600

    Added new exercise

---
+++ 2.1/crash-course-2.cpp
@@ -0,0 +1,29 @@
+//
+//  crash-course-2.cpp
+//  algorithm-study
+//
+//  Created by Francisco Ponce on 11/6/17.
+//  Copyright © 2017 Francisco Ponce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    const size_t n = 2;
+    
+    // Create the array
+    int ** array = new int * [n];
+    
+    for(size_t i=0 ; i<n ; i++)
+        array[i] = new int[n];
+    
+    // Fill the array
+    for(size_t i=0 ; i<n ; i++)
+        for(size_t j=0 ; j<n ; j++)
+            array[i][j] = i * n + j;
+    
+    // Test
+    for(size_t i=0 ; i<n ; i++)
+        for(size_t j=0 ; j<n ; j++)
+            std::cout << "array[" << i << "," << j << "] = " << array[i][j] << std::endl;
+}
